9fbd8008b5c2c9f585cd1b8fd7de5a3ae3819e13
Zach Niemann <czniemann1990@gmail.com>
2018-04-09 13:09:38 -0500

    fixed titlebar bug - images were not loading (not fixed after moving images to category folders

--- src/titlebar.js
+++ src/titlebar.js
@@ -4,8 +4,8 @@ import './titlebar.css'
 const Titlebar = ({ onOpenSettings, onClose })  => {
     return (
         <div id='display-titlebar-container'>
-            <img id='titlebar-menu-icon' onClick={onOpenSettings} src='/images/menu_white.png' alt='failed to load menu_white.png' />
-            <img className='titlebar-icon' id='close-app' onClick={onClose} src='/images/close_white.png' alt='failed to load close_white.png' />
+            <img id='titlebar-menu-icon' onClick={onOpenSettings} src='/images/titlebar_icons/menu-white.svg' alt='failed to load menu icon' />
+            <img className='titlebar-icon' id='close-app' onClick={onClose} src='/images/titlebar_icons/close.svg' alt='failed to load close icon' />
         </div>
     ); 
 }
